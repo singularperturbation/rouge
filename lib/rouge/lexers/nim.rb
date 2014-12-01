b13212351cd667c63d4cc0c119b00ce740e20257
singularperturbation <sloanes.k@gmail.com>
2014-11-30 20:41:56 -0600

    Pop->Pop! and underscorize
    Also added regex from function output 'underscorize'.  Consider
    refactoring as a compiled regexes with opWords, etc.?

--- lib/rouge/lexers/nim.rb
+++ lib/rouge/lexers/nim.rb
@@ -10,7 +10,7 @@ module Rouge
       aliases 'nimrod'
       filenames '*.nim'
 
-      keywords = %w(
+      @@keywords = %w(
         addr as asm atomic bind block break case cast const continue
         converter discard distinct  do elif else end enum except export finally
         for from generic if import include interface iterator let macro method 
@@ -18,18 +18,37 @@ module Rouge
         tuple type using var when while with without yield
       )
 
-      opWords = %w(
+      @@opWords = %w(
         and or not xor shl shr div mod in notin is isnot
       )
 
-      pseudoKeywords = %w(
+      @@pseudoKeywords = %w(
         nil true false
       )
 
-      types = %w(
+      @@types = %w(
        int int8 int16 int32 int64 float float32 float64 bool char range array
        seq set string
       ) 
+
+      def self.underscorize(words)
+        newWords = []
+        new      = ""
+        words.each do |word|
+          word.chars.each do |char|
+            new << (char + "_?")
+          end
+          newWords << new
+          new = ""
+        end
+        return newWords.join("|")
+      end
+
+      state :chars do
+        rule(/\\([\\abcefnrtvl"\']|x[a-fA-F0-9]{2}|[0-9]{1,3})/, Str::Escape)
+        rule(/'/, Str::Char, :pop!)
+        rule(/./, Str::Char)
+      end
 
       state :strings do
         rule(/(?<!\$)\$(\d+|#|\w+)+/, Str::Interpol)
@@ -41,20 +60,25 @@ module Rouge
       state :dqs do
         rule(/\\([\\abcefnrtvl"\']|\n|x[a-fA-F0-9]{2}|[0-9]{1,3})/,
              Str::Escape)
-        rule(/""/, Str, :pop)
+        rule(/""/, Str, :pop!)
         mixin :strings
       end
 
       state :rdqs do
-        rule(/"(?!")/, Str, :pop)
-        rule(/"/,      Str::Escape, :pop)
+        rule(/"(?!")/, Str, :pop!)
+        rule(/"/,      Str::Escape, :pop!)
         mixin :strings
       end
 
       state :tdqs do
-        rule(/"""(?!")/, Str, :pop)
+        rule(/"""(?!")/, Str, :pop!)
         mixin :strings
         mixin :nl
+      end
+
+      state :funcname do
+        rule(/((?![\d_])\w)(((?!_)\w)|(_(?!_)\w))*/, Name::Function, :pop!)
+        rule(/`.+`/,                                 Name::Function, :pop!)
       end
 
       state :nl do
@@ -68,9 +92,17 @@ module Rouge
         rule(/\.\.|\.|,|\[\.|\.\]|{\.|\.}|\(\.|\.\)|{|}|\(|\)|:|\^|`|;/,
              Punctuation)
 
+        # Strings
         rule(/(?:[\w]+)"/,Str, :rdqs)
         rule(/"""/,       Str, :tdqs)
         rule(/"/,         Str, :dqs)
+
+        # Char
+        rule(/'/, Str::Char, :chars)
+
+        # Keywords
+        rule(%r[(#{Nim.underscorize(@@opWords)})\b], Operator::Word)
+        rule(/(p_?r_?o_?c_?\s)(?![\(\[\]])/, Keyword, :funcname)
 
       end
 
